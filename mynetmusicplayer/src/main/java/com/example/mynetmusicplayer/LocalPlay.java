abe3fabaf4e4bcc0615e5f72a8ce9879d02b6a8e
shush <shush.sec@gmail.com>
2017-05-31 14:06:20 +0800

    fix

---
+++ mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/LocalPlay.java
@@ -0,0 +1,13 @@
+package com.example.mynetmusicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_play);
+    }
+}
--- mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/MainActivity.java
+++ mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.mynetmusicplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.mynetmusicplayer.adapter.SectionsPagerAdapter;
 
 import static com.example.mynetmusicplayer.R.id.container;
 
 public class MainActivity extends AppCompatActivity {
+
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
@@ -31,10 +35,21 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent = new Intent(this,LocalPlay.class);
+        startActivity(intent);
+        overridePendingTransition(android.R.anim.fade_in,android.R.anim.slide_out_right);
+        return true;
+    }
 }
 
 
