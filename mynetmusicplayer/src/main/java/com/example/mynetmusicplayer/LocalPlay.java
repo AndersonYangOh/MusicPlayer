eaf4aed6d2d8b354a99c2d3f9b0ede16b826b50e
shush <shush.sec@gmail.com>
2017-06-02 09:59:08 +0800

    本地音乐播放器雏形

--- mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/LocalPlay.java
+++ mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/LocalPlay.java
@@ -5,7 +5,6 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,12 +13,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.example.mynetmusicplayer.service.PlayerService;
 import com.example.mynetmusicplayer.utils.Song;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -28,25 +29,34 @@ import java.util.List;
 public class LocalPlay extends AppCompatActivity {
 
     private static final int PLAY_MSG = 0;
-    MediaPlayer mMediaPlayer;
+    private int ISPLAYING = 0; // 0播放，1暂停
+    private int currentPosition = -1 ;  //当前播放的歌曲ID
+
+    PlayerService musicService;
     SimpleAdapter mAdapter;
     ListView mMusicList;
-    List<Song> songList;
+    public static List<Song> songList;
+    Song song;
+    Intent intent;
+    ImageButton ibPlay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_play);
-        mMusicList= (ListView) findViewById(R.id.music_list);
+        mMusicList = (ListView) findViewById(R.id.music_list);
+        ibPlay = (ImageButton) findViewById(R.id.play_music);
+        ImageButton ibPre = (ImageButton) findViewById(R.id.previous_music);
+        ImageButton ibNext = (ImageButton) findViewById(R.id.next_music);
         mMusicList.setOnItemClickListener(new MusicListItemClickListener());
-
+        musicService = new PlayerService();
 
         //运行时申请权限
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
-        }else{
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
             List<Song> songList = getSongList();
             setListAdapter(songList);
         }
@@ -65,24 +75,18 @@ public class LocalPlay extends AppCompatActivity {
             long id = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media._ID));
             String title = cursor.getString((cursor
                     .getColumnIndex(MediaStore.Audio.Media.TITLE)));//音乐标题
-            String artist = cursor.getString(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.ARTIST));//艺术家
             long duration = cursor.getLong(cursor
                     .getColumnIndex(MediaStore.Audio.Media.DURATION));//时长
-            long size = cursor.getLong(cursor
-                    .getColumnIndex(MediaStore.Audio.Media.SIZE));  //文件大小
             String url = cursor.getString(cursor
                     .getColumnIndex(MediaStore.Audio.Media.DATA));   //文件路径
             int isMusic = cursor.getInt(cursor
                     .getColumnIndex(MediaStore.Audio.Media.IS_MUSIC));//是否为音乐
 
-            if (isMusic !=0 ) {
+            if (isMusic != 0) {
                 song.setSongID(id);
                 song.setSongTitle(title);
                 song.setSongID(id);
-                song.setArtist(artist);
                 song.setDuration(duration);
-                song.setSize(size);
                 song.setUrl(url);
                 songList.add(song);
 
@@ -99,27 +103,123 @@ public class LocalPlay extends AppCompatActivity {
             HashMap<String, String> map = new HashMap<>();
             map.put("title", mp3Info.getSongTitle());
             map.put("artist", mp3Info.getArtist());
-            map.put("duration", String.valueOf(mp3Info.getDuration()));
+            map.put("duration", String.valueOf(formatTime(mp3Info.getDuration())));
             map.put("size", String.valueOf(mp3Info.getSize()));
             map.put("url", mp3Info.getUrl());
             mp3list.add(map);
         }
         mAdapter = new SimpleAdapter(this, mp3list,
-                R.layout.music_list_item, new String[]{"title", "duration","size"},
-                new int[]{R.id.title, R.id.duration,R.id.size});
+                R.layout.music_list_item, new String[]{"title", "duration"},
+                new int[]{R.id.title, R.id.duration});
         mMusicList.setAdapter(mAdapter);
     }
 
+    //上一首、播放、下一首
+    public void onclick(View view) {
+        switch (view.getId()) {
+            case R.id.play_music:
+                musicService.playOrPause();
+                //更改图片
+                if (ISPLAYING == 1) {
+                    ibPlay.setBackgroundResource(R.drawable.pause_dark);
+                    ISPLAYING = 0;
+                } else {
+                    ibPlay.setBackgroundResource(R.drawable.play_dark);
+                    ISPLAYING = 1;
+                }
+                break;
+            case R.id.previous_music:
+                if (currentPosition-1 <= 0) {
+                    try {
+                        musicService.mediaPlayer.setDataSource(
+                                songList.get(currentPosition - 1).getUrl());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    musicService.mediaPlayer.reset();
+                    musicService.playOrPause();
+                }
+                break;
+            case R.id.next_music:
+                if (currentPosition+1 >= songList.size()){
+                    try {
+                        musicService.mediaPlayer.setDataSource(
+                                songList.get(currentPosition+1).getUrl());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    musicService.mediaPlayer.reset();
+                    musicService.playOrPause();
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    //listview点击事件
     private class MusicListItemClickListener implements android.widget.AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            if (mMusicList != null){
-                Song song = songList.get(position);
-                Intent intent = new Intent(LocalPlay.this,PlayerService.class);
-                intent.putExtra("url",song.getUrl());
-                intent.putExtra("MSG", PLAY_MSG);
-                startService(intent);
+            if (mMusicList != null) {
+                if (currentPosition == -1){                 //第一次点击列表
+                    song = songList.get(position);
+                    intent = new Intent(LocalPlay.this, PlayerService.class);
+                    intent.putExtra("url", song.getUrl());
+                    startService(intent);
+                    currentPosition = position;
+                    ISPLAYING = 0;
+                    ibPlay.setBackgroundResource(R.drawable.pause_dark);
+                }else if (currentPosition == position){  //点击了当前正在播放的歌曲
+                    //更改图片
+                    if (ISPLAYING == 1) {
+                        ibPlay.setBackgroundResource(R.drawable.pause_dark);
+                        ISPLAYING = 0;
+                        musicService.playOrPause();
+                    } else {
+                        ibPlay.setBackgroundResource(R.drawable.play_dark);
+                        ISPLAYING = 1;
+                        musicService.playOrPause();
+                    }
+                }else {                             //点击了非当前播放的歌曲
+                    song = songList.get(position);
+                    intent = new Intent(LocalPlay.this, PlayerService.class);
+                    intent.putExtra("url", song.getUrl());
+                    startService(intent);
+                    currentPosition = position;
+                    ISPLAYING = 0;
+                    ibPlay.setBackgroundResource(R.drawable.pause_dark);
+                }
+
             }
         }
+    }
+
+    /**
+     * 格式化时间，将毫秒转换为分:秒格式
+     *
+     * @param time
+     * @return
+     */
+    public static String formatTime(long time) {
+        String min = time / (1000 * 60) + "";
+        String sec = time % (1000 * 60) + "";
+        if (min.length() < 2) {
+            min = "0" + time / (1000 * 60) + "";
+        } else {
+            min = time / (1000 * 60) + "";
+        }
+        if (sec.length() == 4) {
+            sec = "0" + (time % (1000 * 60)) + "";
+        } else if (sec.length() == 3) {
+            sec = "00" + (time % (1000 * 60)) + "";
+        } else if (sec.length() == 2) {
+            sec = "000" + (time % (1000 * 60)) + "";
+        } else if (sec.length() == 1) {
+            sec = "0000" + (time % (1000 * 60)) + "";
+        }
+        return min + ":" + sec.trim().substring(0, 2);
     }
 }
--- mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/service/PlayerService.java
+++ mynetmusicplayer/src/main/java/com/example/mynetmusicplayer/service/PlayerService.java
@@ -1,50 +1,145 @@
 package com.example.mynetmusicplayer.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.mynetmusicplayer.MainActivity;
+import com.example.mynetmusicplayer.R;
 
 import java.io.IOException;
 
 public class PlayerService extends Service {
-    private static final int PLAY_MSG = 0;
-    private static final int PAUSE_MSG = 1;
-    private static final int STOP_MSG = 2;
-    private MediaPlayer mediaPlayer =  new MediaPlayer();       //媒体播放器对象
+    private static final int PLAY_MSG = 0;    //播放
+    private static final int PAUSE_MSG = 1;   //暂停
+    private static final int STOP_MSG = 2;    //停止
+    public static MediaPlayer mediaPlayer = new MediaPlayer();//媒体播放器对象
     private String path;                        //音乐文件路径
-    private boolean isPause;                    //暂停状态
+
     public PlayerService() {
+    }
+
+
+    public final IBinder binder = new MyBinder();
+
+    public class MyBinder extends Binder {
+        PlayerService getService() {
+            return PlayerService.this;
+        }
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(mediaPlayer.isPlaying()) {
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
         }
         path = intent.getStringExtra("url");
         int msg = intent.getIntExtra("MSG", 0);
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        if(msg == PLAY_MSG) {
+        if (msg == PLAY_MSG) {
             try {
+                mediaPlayer.reset();
                 mediaPlayer.setDataSource(path);
                 mediaPlayer.prepare();
             } catch (IOException e) {
                 e.printStackTrace();
             }
             mediaPlayer.start();
-        } else if(msg == PAUSE_MSG) {
+        } else if (msg == PAUSE_MSG) {
             mediaPlayer.pause();
-        } else if(msg == STOP_MSG) {
+        } else if (msg == STOP_MSG) {
             mediaPlayer.stop();
         }
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void playOrPause() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        } else {
+            mediaPlayer.start();
+        }
+    }
+
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.prepare();
+                mediaPlayer.seekTo(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void nextMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+                //mediaPlayer.setDataSource(musicDir[musicIndex+1]);
+                // musicIndex++;
+                mediaPlayer.prepare();
+                mediaPlayer.seekTo(0);
+                mediaPlayer.start();
+            } catch (Exception e) {
+                Log.d("hint", "can't jump next music");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void preMusic() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            try {
+                mediaPlayer.reset();
+//                mediaPlayer.setDataSource(musicDir[musicIndex-1]);
+//                musicIndex--;
+                mediaPlayer.prepare();
+                mediaPlayer.seekTo(0);
+                mediaPlayer.start();
+            } catch (Exception e) {
+                Log.d("hint", "can't jump pre music");
+                e.printStackTrace();
+            }
+        }
     }
 }
